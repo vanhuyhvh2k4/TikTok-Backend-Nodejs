605bc5279175dda1e00e72dc00d3b6383c1a8c7c
anhhuydepzai <anhhuydepzai@gmail.com>
2023-03-11 10:18:49 +0700

    create sever for tiktok react

--- src/app/controllers/NewController.js
+++ src/app/controllers/NewController.js
@@ -1,8 +1,25 @@
+const User = require('../models/User.js');
+
 class NewController {
 
     // [GET] /news
-    index(req, res) {
-        res.render('news');
+    index(req, res, next) {
+        const keyword = req.query.q;
+        const regex = new RegExp(`.*${keyword}`, 'i');
+
+        if (keyword) {
+            User.find({ $or:[{ "fullName": regex }, { "nickName": regex }]})
+            .lean()
+            .limit(5)
+            .then((users) => {
+                    if (users) {
+                        const responseData = { data: users };
+                        res.json(responseData);
+                    }
+                })
+                .catch(next);
+        }
+
     }
 
     // [GET] /news/:slug
